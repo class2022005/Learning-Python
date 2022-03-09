02e1d5117e9ac425374c4c5f325c75d50559b1ce
HR <h@rosso.ws>
2022-03-09 13:39:33 -0500

    More exercises

---
+++ ex012.py
@@ -0,0 +1,4 @@
+# Give 5% discount to your customers
+InitialPrice = float(input('Enter the full price for your product:'))
+SalePrice = InitialPrice * 0.95
+print(f'The sale price with 5% discount is USD {SalePrice}')
---
+++ ex013.py
@@ -0,0 +1,3 @@
+CurrentSalary = float(input('What is your current salary?'))
+SalaryWithRaise = CurrentSalary * 1.15
+print(f'After your excellent performance, your new salary will be USD {SalaryWithRaise}')
